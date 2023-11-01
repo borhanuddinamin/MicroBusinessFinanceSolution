275af14ab6de9ee27dd3e259c7e70ac758510c85
borhanuddinamin <borhanuddinamin@gmail.com>
2023-11-01 23:09:03 +0600

    Project structure done

---
+++ Src/MBS.EmailService/Program.cs
@@ -0,0 +1,10 @@
+using MBS.EmailService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
